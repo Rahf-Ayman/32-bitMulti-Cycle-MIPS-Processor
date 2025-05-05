842f2567a836227f45f404a03e8e6dd9dad2e5ee
Sahar-Elgebaly <saharelgebaly299@gmail.com>
2025-05-05 15:32:05 +0300

    Only One MUX2To1

--- code/Mux2To1.vhd
+++ code/Mux2To1.vhd
@@ -1,16 +1,19 @@
 library ieee;
 use ieee.std_logic_1164.all;
 
-entity Mux2To1_32 is
+entity Mux2To1 is
+	generic(
+	n:integer:=32
+	);
 	port(
-	in0:in std_logic_vector(31 downto 0); 
-	in1:in std_logic_vector(31 downto 0);
+	in0:in std_logic_vector(n-1 downto 0); 
+	in1:in std_logic_vector(n-1 downto 0);
 	selector: in std_logic;
 	output:out std_logic_vector(31 downto 0)
 	);
 end entity;
 
-architecture behave of Mux2To1_32 is
+architecture behave of Mux2To1 is
 begin	
 	with selector select 
 	output<= in0 when '0',
