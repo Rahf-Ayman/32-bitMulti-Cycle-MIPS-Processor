13e58bbc747b6ce3730527ab1ccb63d837f7626e
Rahf Ayman <rahf.family@gmail.com>
2025-05-09 00:21:01 +0300

    control unit connection

--- code/Connection.vhd
+++ code/Connection.vhd
@@ -19,10 +19,41 @@ architecture behavior of connection is
 
 -- component of control unit
 	-- here
+	component ControlTop is
+	port(
+	    -- input
+	    CLK : in std_logic;
+		reset : in std_logic;
+		func : in std_logic_vector(5 downto 0);
+		Op : in std_logic_vector(5 downto 0);
+		
+		-- output
+		operation : out std_logic_vector(3 downto 0);
+		RegDst : out std_logic; 
+		RegWrite : out std_logic;
+		ALUSrcA : out std_logic;
+		MemRead : out std_logic;
+		MemWrite : out std_logic;
+		MemtoReg :out std_logic;
+		IorD : out std_logic;
+		IRWrite :out std_logic;
+		PCWrite : out std_logic;
+		PCWriteCond : out std_logic;
+		ALUSrcB : out std_logic_vector (1 downto 0);
+		PCSource : out std_logic_vector (1 downto 0)
+	);
+end component;
 --
 
 -- internal signal
-	-- here
+-- here
+    signal IROut : std_logic_vector(31 downto 0);
+	signal IRWrite , PCWriteCond , PCWrite 
+	, IorD , MemtoReg , MemWrite , MemRead 
+	, ALUSrcA , RegWrite , RegDst : std_logic;
+	signal ALUSrcB , PCSource : std_logic_vector(1 downto 0);
+	signal operation : std_logic_vector(3 downto 0);
+	
 --
 begin
 -- port map of register
@@ -34,7 +65,13 @@ begin
 --
 
 -- port map of control unit
-	-- here
+-- here
+CTRL : ControlTop port map (CLK => CLK , reset => reset , Op => IROut(31 downto 26) , func => IROut(5 downto 0)
+							,IRWrite => IRWrite , PCWriteCond => PCWriteCond , PCWrite => PCWrite
+							,IorD => IorD , MemtoReg => MemtoReg , MemWrite => MemWrite , MemRead => MemRead
+							,ALUSrcA => ALUSrcA , RegWrite => RegWrite , RegDst => RegDst , ALUSrcB => ALUSrcB
+							,PCSource => PCSource , operation => operation);
+
 --	
 end behavior;
 	
